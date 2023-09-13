ae2cbbb5f4fcc389ec4f56778127072dd539e186
Eoin Murphy <murphy.eoin93@gmail.com>
2023-09-13 06:47:20 +0000

    collecte static files

---
+++ goodtaco/staticfiles/js/script.2d83dfabbdb8.js
@@ -0,0 +1,48 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const dropdown = document.querySelector('.dropdown');
+const navbar = document.querySelector('.navbar');
+
+// Hide the dropdown initially
+dropdown.style.display = 'none';
+
+toggleBtn.addEventListener('click', () => {
+    dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+});
+
+let prevScrollPos = window.pageYOffset;
+let scrollDelay = 0; // Initialize scroll delay
+let hasScrolled = false; // Flag to track if the user has scrolled 100px
+
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.pageYOffset;
+
+    // Calculate the scroll difference between the current and previous scroll positions
+    const scrollDifference = Math.abs(currentScrollPos - prevScrollPos);
+
+    if (scrollDifference >= 50) {
+        // The user has scrolled at least 50px
+        hasScrolled = true;
+    }
+
+    if (hasScrolled) {
+        // Apply the scroll-based delay only if the user has scrolled 100px or more
+        clearTimeout(scrollDelay); // Clear any existing timeout
+        scrollDelay = setTimeout(() => {
+            if (currentScrollPos > prevScrollPos) {
+                // Scrolling down
+                navbar.style.transform = 'translateY(-100%)';
+                navbar.style.opacity = '0';
+            } else {
+                // Scrolling up
+                navbar.style.transform = 'translateY(0)';
+                navbar.style.opacity = '1';
+            }
+            prevScrollPos = currentScrollPos;
+            hasScrolled = false; // Reset the flag
+        }, 10);
+    }
+});
+
+toggleBtn.addEventListener('click', () => {
+    dropdown.classList.toggle('open');
+});
